4bae028870f4758c2c503f2ce9f531eb5761992e
Manimaran T <maran.mallow@gmail.com>
2020-08-08 15:38:47 +0530

    Add files via upload

--- test/com/fullcreative/mycollection/test/MyStackTest.java
+++ test/com/fullcreative/mycollection/test/MyStackTest.java
@@ -14,32 +14,59 @@ import org.junit.Assert;
 
 public class MyStackTest {
 
-	Stack<Character> stk;
-	List expectedList;
+	static Stack<Character> stk;
+	static List<Character> expectedList;
+	static List<Character> expectedList1;
 
-	@Before
-	public void beforeTest() {
-		stk = new Stack<>();
-		stk.push('1');
-		stk.push('2');
-		stk.push('4');
-		stk.push('8');
+	@BeforeClass
+	public static void beforeTest() {
+		
 
 		expectedList = new Stack();
 		expectedList.add('8');
 		expectedList.add('4');
 		expectedList.add('2');
 		expectedList.add('1');
+		
+		expectedList1 = new Stack();
+		expectedList1.add('8');
+		expectedList1.add('4');
+		expectedList1.add('2');
+		expectedList1.add(null);
+	
 
 	}
 
 	@Test
 	public void reverseProgTest() {
+		
+		stk = new Stack<>();
+		stk.push('1');
+		stk.push('2');
+		stk.push('4');
+		stk.push('8');
 
 		List<Character> l = MyStack.reverseProg(stk);
 
 		assertNotNull(l);
 		assertEquals(expectedList, l);
+
+	}
+	
+	@Test
+	public void reverseProgTest2() {
+		
+		
+		stk = new Stack<>();
+		stk.push(null);
+		stk.push('2');
+		stk.push('4');
+		stk.push('8');
+
+		List<Character> l = MyStack.reverseProg(stk);
+
+		assertNotNull(l);
+		assertEquals(expectedList1, l);
 
 	}
 
