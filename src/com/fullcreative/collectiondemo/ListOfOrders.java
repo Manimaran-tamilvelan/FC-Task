9e7dc76470e84c78661f4facfa4876bb4ab23bc4
Manimaran T <maran.mallow@gmail.com>
2020-08-09 21:35:57 +0530

    Add files via upload

--- src/com/fullcreative/collectiondemo/ListOfOrders.java
+++ src/com/fullcreative/collectiondemo/ListOfOrders.java
@@ -7,27 +7,44 @@ public class ListOfOrders {
 
 	static int i = 1;
 
+	static int loop;
+	static Scanner sc;
+	static List<Map<Integer, String>> l;
+
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 
-		List<Map<Integer, String>> l = new Vector<>();
+		l = new ArrayList<>();
 
 		System.out.println("How Many order you want to place:");
-		int loop = sc.nextInt();
+		loop = sc.nextInt();
 
+		getList(loop);
+
+	}
+
+	public static void getList(int loop) {
 		for (int i = 1; i <= loop; i++) {
 
 			System.out.print("Enter " + i + "st order: ");
 			String s = sc.next();
-
-			OrderDetail.placeOrder(s);
-			l.add(OrderDetail.getter());
+			
+			l.addAll(list(s));
 
 		}
 
 		System.out.println("Order Placed Note Order ID");
 		System.out.println(l);
+	}
+
+	public static List<Map<Integer, String>> list(String s) {
+		 List<Map<Integer, String>> l1  = new ArrayList<>();;
+		
+		OrderDetail.placeOrder(s);
+		l1.add(OrderDetail.getter());
+		
+		return l1;
 
 	}
 
--- src/com/fullcreative/collectiondemo/OrderDetail.java
+++ src/com/fullcreative/collectiondemo/OrderDetail.java
@@ -1,17 +1,18 @@
 package com.fullcreative.collectiondemo;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
-class OrderDetail {
+public class OrderDetail {
 	
 	private static Map<Integer, String> m;
 	private static int oid = 1001;
 
 	public static void placeOrder(String s) {
 
-		m = new TreeMap();
+		m = new HashMap();
 		m.put(oid, s);
 		oid++;
 
